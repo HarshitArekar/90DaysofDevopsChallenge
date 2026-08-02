2b6437c66413788b75123db9636c8bfbb1033b90
Harshit-Arekar <arekarharshit61@gmail.com>
2026-08-02 11:20:13 +0530

    Create disk_check.sh

---
+++ Day18/scripts/disk_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_memory () {
+echo "============= Memory Usage ========="
+free -h
+}
+
+
+check_storage () {
+echo "============= Disk  Usage ========="
+df -h
+}
+
+check_memory
+check_storage
+~                                                                                                                                                                  
