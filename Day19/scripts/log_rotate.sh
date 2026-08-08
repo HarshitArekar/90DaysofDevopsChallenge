322e86d71051b77cb0bad48e7c1ae2d0ab41361b
Harshit-Arekar <arekarharshit61@gmail.com>
2026-08-08 09:22:22 +0530

    Refactor log_rotate.sh for better organization
    Refactored log rotation script to improve structure and readability.

--- Day19/scripts/log_rotate.sh
+++ Day19/scripts/log_rotate.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
 
-set -eu
+perform_rotation (){
 
-usage() {
-	echo "Usage: ./log_rotate.sh /var/log/app_name"
-	echo "Example: ./log_rotate.sh /var/log/apache2"
-	exit 1
+        backups=($(ls -t "${backup_dir}/backups_"*.zip 2>/dev/null))
+
+
+        if  [[ "${#backups[@]}" -gt 5 ]]; then
+                echo " Performing rotation for 5 days "
+        fi
+
+
+        backups_to_remove=("${backups[@]:5}")
+
+        echo "${backups_to_remove[@]}" 
+
+        for backups in "${backups_to_remove[@]}"
+        do
+                rm -f ${backups}
+        done
 }
 
-if [ "$#" -eq 0 ]; then
-	usage
-fi
 
-dir=$1
 
-check_dir() {
-	[ -d "$dir" ] || {
-		echo "Directory $dir does not exists"
-	        exit 1
-}
-}
-
-gzip_count=0
-delete_count=0
-
-compress() {
-	file_list=$(find "$dir" -type f -name "*.log" -mtime +7)
-
-	for file in $file_list; do
-		gzip "$file"
-		gzip_count=$((gzip_count + 1))
-	done
-}
-
-delete_log() {
-	zip_files=$(find "$dir" -type f -name "*.gz" -mtime +30)
-
-	for file in $zip_files; do
-		rm "$file"
-		delete_count=$((delete_count + 1))
-	done
-}
-
-check_dir
-compress
-delete_log
-
-echo "Total log files zipped : $gzip_count"
-echo "total zip files deleted : $delete_count"
+perform_rotation
