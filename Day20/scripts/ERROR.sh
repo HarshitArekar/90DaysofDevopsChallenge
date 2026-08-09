b6a8ca273338bfbd001be11a0dfa8509029a9825
Harshit-Arekar <arekarharshit61@gmail.com>
2026-08-09 07:49:59 +0530

    Add ERROR.sh to check log file for errors
    This script checks for the existence of a log file and counts the number of error lines in it.

---
+++ Day20/scripts/ERROR.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+        echo "error please proivde file path"
+        echo "Usage ./script.sh <file path>"
+        exit 1
+fi
+
+#store log file path
+
+log_file=$1
+
+#check if file exits or not 
+
+if [[ ! -f $log_file ]]; then
+        echo " error: log file doesn't exist $log_file"
+fi
+
+echo " log file found $log_file"  
+
+# how many lines in a log file indicate a problem
+#
+error_count=$( grep -Ei 'ERROR|Failed' "$log_file" | wc -l)
+echo "Total error count: $error_count "
+
+
+#Search for lines containing the keyword ERROR
+
+error=$(grep -n "ERROR" "$log_file")
+
+echo " $error" 
