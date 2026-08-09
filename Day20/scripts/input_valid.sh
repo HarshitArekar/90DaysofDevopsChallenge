dc7d05f3a0f1104f3de835a18a45fb779f75c71c
Harshit-Arekar <arekarharshit61@gmail.com>
2026-08-09 07:47:29 +0530

    Create input_valid.sh for log file validation
    Add input validation script for checking log file existence.

---
+++ Day20/scripts/input_valid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [[ $# -eq 0 ]]; then
+        echo "error please proivde file path"
+        echo "Usage ./script.sh <file path>"
+        exit 1
+fi
+
+#store log file path
+
+log_file=$1
+
+#check if file exits or not 
+
+if [[ ! -f $log_file ]]; then
+        echo " error: log file doesn't exist $log_file"
+fi
+
+echo " log file found $log_file"  
